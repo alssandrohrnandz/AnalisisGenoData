4ac6bf56af68a5ee721fc3739b6355a7e66c9a88
Alessandro Hernández <90350757+alssandrohrnandz@users.noreply.github.com>
2024-09-02 10:42:47 -0600

    Create DownloadData.sh

---
+++ DownloadData.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p ../EigenFiles
+curl -O -L https://reichdata.hms.harvard.edu/pub/datasets/amh_repo/curated_releases/V54/V54.1.p1/SHA
+curl -O -L https://reichdata.hms.harvard.edu/pub/datasets/amh_repo/curated_releases/V54/V54.1.p1/SHA
+curl -O -L https://reichdata.hms.harvard.edu/pub/datasets/amh_repo/curated_releases/V54/V54.1.p1/SHA
+curl -O -L https://reichdata.hms.harvard.edu/pub/datasets/amh_repo/curated_releases/V54/V54.1.p1/SHA
